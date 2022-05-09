9c131d79feb0090811d63073eb39385b58dfa40a
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-09 18:46:05 +0200

    edit in serverozzo

--- packages/serverozzo/lib/serverozzo.js
+++ packages/serverozzo/lib/serverozzo.js
@@ -5,7 +5,7 @@ const godone = require("@my-project/library");
 module.exports = serverozzo;
 
 function serverozzo() {
-  console.log("Parte il serverozzo...");
+  console.log("Parte il serverozzo....");
   godone();
 }
 
